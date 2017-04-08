e5e1731346d0981f1c7e77f315f5f8788d1ab2a5
Danny Kim <dannykim@Dannys-MacBook-Air.local>
2017-04-07 23:44:41 -0700

    button

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 $( document ).ready(function() {
     console.log("TEST!"); 
+    $("#click").click(function(){
+         var somearray = [0,2];
+         
+         console.log(somearray); 
+    });
 });
